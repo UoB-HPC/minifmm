82aa0b80e0d9c21e09abff2d078f368f809846a5
Patrick Atkinson <pa13269@my.bristol.ac.uk>
2017-04-04 12:52:43 +0100

    prints out how many threads are running

--- omp/main.c
+++ omp/main.c
@@ -11,26 +11,6 @@
 #include "verify.h"
 #include "initialise.h"
 
-// void print_tree(t_node* root, t_node* rroot)
-// {
-//     printf("node %zu has %zu points\n", root-rroot, root->num_points);
-//     for (size_t i = 0; i < root->num_children; ++i) print_tree(root->child[i], rroot);
-// }
-
-// void direct_tree(t_fmm_options* options)
-// {
-//     printf("num nodes = %zu\n", options->num_nodes);
-//     for (size_t i = 0; i < options->num_nodes; ++i)
-//     {
-//         for (size_t j = 0; j < options->num_nodes; ++j)
-//         {
-//             if (is_leaf(&options->root[i]) && is_leaf(&options->root[j])) p2p(options, &options->root[i], &options->root[j]);
-//         }
-//     }
-// }
-
-
-
 extern int m2l_calls;
 int main(int argc, char** argv)
 {
@@ -39,29 +19,18 @@ int main(int argc, char** argv)
 
     t_timer timer;
     start(&timer);
-    #ifdef _OPENMP
+
     #pragma omp parallel
     #pragma omp single
-    #endif
-    perform_fmm(&options);
-    // direct_tree(&options);
+    {
+        printf("running computation on %d threads\n", omp_get_num_threads());
+        perform_fmm(&options);
+    }
     stop(&timer);
     timer_print(&timer, "fmm");
 
-    // check(&options);
     TYPE a_err, p_err;
     verify(&options, &a_err, &p_err);
     printf("%f %f\n", a_err, p_err);
     printf("m2l calls = %d\n", m2l_calls);
-    // printf("num wrong = %d\n", num_wrong);
-    // print_tree(options.root, options.root);
-
-
-    // for (size_t i = 0; i < options.num_points; ++i)
-    // {
-    //     for (int d = 0; d < 3; ++d) 
-    //         printf("%f ", options.points_ordered[d*options.num_points+i]);
-    //     printf("\n");
-    // }
-
 }
